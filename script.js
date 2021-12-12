bc34b718d2809b4a4f6e587d03d07bd277179650
duc12052001 <96016484+duc12052001@users.noreply.github.com>
2021-12-12 23:45:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed(".typing",{
+    strings:["Vật Lý Kỹ Thuật","Đại học Bách Khoa","Vật Lý Kỹ Thuật","Đại học Bách Khoa"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+;
